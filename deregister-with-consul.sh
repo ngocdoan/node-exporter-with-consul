aaa6327ff975d81f45fd36b80a1e5b591499cdc3
ngocdoan <vuongngocdoan@gmail.com>
2025-06-08 21:52:22 +0700

    Create deregister-with-consul.sh

---
+++ deregister-with-consul.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Prompt the user for the Consul IP address
+echo "Please enter the Consul server IP address (default: 192.168.135.249):"
+read CONSUL_IP
+
+
+# Prompt the user for the Consul server port (default to 8500 if empty)
+echo "Please enter the Consul server port (default: 8500):"
+read CONSUL_PORT
+
+# If the user didn't enter a port, default to 8500
+if [ -z "$CONSUL_PORT" ]; then
+  CONSUL_PORT="8500"
+fi
+
+# Combine IP and port to form the Consul server URL
+CONSUL_SERVER="http://$CONSUL_IP:$CONSUL_PORT"
+
+# Get the hostname of the machine
+HOSTNAME=$(hostname)
+
+# Send the request to deregister the service from Consul
+response=$(curl -s -o /dev/null -w "%{http_code}" -X PUT "$CONSUL_SERVER/v1/agent/service/deregister/$HOSTNAME")
+
+# Check the HTTP status code returned
+if [ "$response" -eq 200 ]; then
+  echo "The service with ID $HOSTNAME has been successfully deregistered from Consul."
+else
+  echo "Failed to deregister the service with ID $HOSTNAME from Consul. Error code: $response"
+fi
